811ae3886f767379801b54140304729ef44cc37e
Paul Adelaja <pauladelaja54@gmail.com>
2024-05-28 15:05:23 +0100

    adjusted server.ts

--- server.ts
+++ server.ts
@@ -1,8 +1,8 @@
-// export const server = "https://elearningapi-rjgb.onrender.com/api/v1"
+export const server = "https://elearningapi-rjgb.onrender.com/api/v1"
 
-// export const client = "https://e-learning-client-phi.vercel.app"
+export const client = "https://e-learning-client-phi.vercel.app"
 
 
-export const server = "http://localhost:8000/api/v1"
+// export const server = "http://localhost:8000/api/v1"
 
-export const client = "http://localhost:3000"
+// export const client = "http://localhost:3000"
