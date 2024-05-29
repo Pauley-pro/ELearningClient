212daa5b049de3bf70288d59d53358c248db235b
Paul Adelaja <pauladelaja54@gmail.com>
2024-05-29 10:56:16 +0100

    still on the session issue

--- next-auth.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    // Configure auth settings
-    auth: {
-      // Enable SSR support
-      ssr: true,
-    },
-  };
--- next.config.js
+++ next.config.js
@@ -3,6 +3,11 @@ const nextConfig = {
     images: {
         domains: ['res.cloudinary.com','randomuser.me'],
     },
+    target: 'serverless',
+    auth: {
+        // Configure auth settings
+        ssr: true,
+      },
 }
 
 module.exports = nextConfig
