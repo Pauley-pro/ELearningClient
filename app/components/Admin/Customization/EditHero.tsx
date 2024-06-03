394e15de7b0077c4e967445f43a6d664770501d3
Paul Adelaja <pauladelaja54@gmail.com>
2024-06-03 14:03:31 +0100

    Modidied EditHero

--- app/components/Admin/Customization/EditHero.tsx
+++ app/components/Admin/Customization/EditHero.tsx
@@ -7,38 +7,38 @@ import { AiOutlineCamera } from 'react-icons/ai';
 
 type Props = {}
 
-const EditHero:FC<Props> = (props: Props) => {
+const EditHero: FC<Props> = (props: Props) => {
     const [image, setImage] = useState("");
     const [title, setTitle] = useState("");
     const [subTitle, setSubtitle] = useState("");
-    const {data, refetch} = useGetHeroDataQuery("Banner", {
+    const { data, refetch } = useGetHeroDataQuery("Banner", {
         refetchOnMountOrArgChange: true,
     });
-    const [editLayout,{isLoading, isSuccess, error}] = useEditLayoutMutation();
+    const [editLayout, { isLoading, isSuccess, error }] = useEditLayoutMutation();
 
     useEffect(() => {
-        if(data){
+        if (data) {
             setTitle(data?.layout?.banner.title);
             setSubtitle(data?.layout?.banner.subTitle);
             setImage(data?.layout?.banner?.image?.url);
         }
-        if(isSuccess){
+        if (isSuccess) {
             refetch();
             toast.success("Hero updated successfully!")
         }
-        if(error){
-            if("data" in error){
+        if (error) {
+            if ("data" in error) {
                 const errorData = error as any;
                 toast.error(errorData?.data?.message);
             }
         }
     }, [data, isSuccess, error, refetch]);
 
-    const handleUpdate = (e:any) => {
+    const handleUpdate = (e: any) => {
         const file = e.target.files?.[0]
         if (file) {
             const reader = new FileReader();
-            reader.onload = (e:any) => {
+            reader.onload = (e: any) => {
                 if (reader.readyState === 2) {
                     setImage(e.target.result as string);
                 }
@@ -49,7 +49,7 @@ const EditHero:FC<Props> = (props: Props) => {
 
     const handleEdit = async () => {
         await editLayout({
-            type:"Banner",
+            type: "Banner",
             image,
             title,
             subTitle
@@ -60,27 +60,27 @@ const EditHero:FC<Props> = (props: Props) => {
         <>
             <div className="w-full 1000px:flex items-center">
                 <div className="absolute top-[100px] 1000px:top-[unset] 1500px:h-[700px] 1500px:w-[700px] 1100px:h-[500px] 1100px:w-[500px] h-[50vh] w-[50vh] hero_animation rounded-[50%] 1100px:left-[18rem] 1500px:left-[21rem]"></div>
-                <div className="1000px:w-[40%] flex 1000px:min-h-screen items-center justify-end pt-[70px] 1000px:pt-[0] z-10">
-                    <div className="relative flex items-center justify-end">
+                <div className="1000px:flex 1000px:min-h-screen items-center justify-between pt-[70px] 1000px:pt-[0] z-10">
+                    <div className="relative flex items-center justify-center 1000px:w-[40%]">
                         <Image
-                            src={image} 
-                            alt="" 
-                            className="object-contain 1100px:max-w-[90%] w-[90%] 1500px:max-w-[85%] h-[auto] z-[10]" 
+                            src={image}
+                            alt=""
+                            className="object-contain 1100px:max-w-[90%] w-[90%] 1500px:max-w-[85%] h-[auto] z-[10]"
                         />
-                        <input 
-                            type="file" 
-                            name="" 
-                            id="banner" 
-                            accept="image/*" 
-                            onChange={handleUpdate} 
-                            className="hidden" 
+                        <input
+                            type="file"
+                            name=""
+                            id="banner"
+                            accept="image/*"
+                            onChange={handleUpdate}
+                            className="hidden"
                         />
                         <label htmlFor="banner" className="absolute bottom-0 right-0 z-20">
                             <AiOutlineCamera className="dark:text-white text-black text-[18px] cursor-pointer" />
                         </label>
                     </div>
                     <div className="1000px:w-[60%] flex flex-col items-center 1000px:mt-[0px] text-center 1000px:text-left mt-[150px]">
-                        <textarea 
+                        <textarea
                             className="dark:text-white resize-none text-[#000000c7] text-[30px] px-3 w-full 1000px:text-[60px] 1500px:text-[70px]"
                             placeholder="Improve Your Online Learning Experience Better Instantly"
                             value={title}
@@ -88,7 +88,7 @@ const EditHero:FC<Props> = (props: Props) => {
                             rows={4}
                         ></textarea>
                         <br />
-                        <textarea 
+                        <textarea
                             className="dark:text-[#edfff4] text-[#000000ac] font-Josefin font-[600] text-[18px] 1500px:!w-[55%] 1100px:!w-[78%] bg-transparent"
                             placeholder="We have 40k+ Online registered students. Find your desired courses from them."
                             value={title}
@@ -99,17 +99,17 @@ const EditHero:FC<Props> = (props: Props) => {
                         <br />
                         <br />
                         <div className={`${styles.button} !w-[100px] !min-h-[40px] !h-[40px] dark:text-white text-black bg-[#cccccc34]
-                            ${data?.layout?.banner?.title !== title || 
-                                data?.layout?.banner?.subTitle !== subTitle || 
+                            ${data?.layout?.banner?.title !== title ||
+                                data?.layout?.banner?.subTitle !== subTitle ||
                                 data?.layout?.banner?.image?.url !== image
-                            ? "!cursor-pointer !bg-[#42d383]" 
-                            : "!cursor-not-allowed"}
+                                ? "!cursor-pointer !bg-[#42d383]"
+                                : "!cursor-not-allowed"}
                             !rounded absolute bottom-12 right-12`}
                             onClick={
-                                data?.layout?.banner?.title !== title || 
-                                data?.layout?.banner?.subTitle !== subTitle || 
-                                data?.layout?.banner?.image?.url !== image
-                                ? handleEdit : () => null
+                                data?.layout?.banner?.title !== title ||
+                                    data?.layout?.banner?.subTitle !== subTitle ||
+                                    data?.layout?.banner?.image?.url !== image
+                                    ? handleEdit : () => null
                             }
                         >
                             Save
