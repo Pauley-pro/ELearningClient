da3833b02d19045f36f1ebb7bfe639b4e9adf138
Paul Adelaja <pauladelaja54@gmail.com>
2024-06-03 15:31:53 +0100

    Still on the faq

--- app/components/FAQ/FAQ.tsx
+++ app/components/FAQ/FAQ.tsx
@@ -22,7 +22,7 @@ const FAQ = (props: Props) => {
 
 
   return (
-    <div className="min-h-screen">
+    <div className="h-screen">
       <div className="w-[90%] 800px:w-[80%] m-auto">
         <h1 className={`${styles.title} 800px:text-[40px]`}>
           Frequently Asked Question
