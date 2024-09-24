e337c4d34e7ba46732c8dfcf0ee818dbf400d92b
Paul Adelaja <pauladelaja54@gmail.com>
2024-09-24 18:45:20 +0100

    Working on the logout

--- app/components/Profile/SideBarProfile.tsx
+++ app/components/Profile/SideBarProfile.tsx
@@ -26,7 +26,7 @@ const SideBarProfile:FC<Props> = ({user, active, avatar, setActive }) => {
     try {
       Cookies.remove("userId");
       Cookies.remove("user_token");
-      const response = await fetch(`${server}/api/vi/logout`, {
+      const response = await fetch(`${server}/api/v1/logout`, {
         method: "GET",
       });
 
