7d96137e5ce13c2bc6458c53fda5e7f427e71b71
Adelaja Paul <pauladelaja54@gmail.com>
2024-11-06 11:10:13 +0100

    Adjusted z-index

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -85,8 +85,8 @@ const Header: FC<Props> = ({ activeItem, setOpen, route, open, setRoute }) => {
                     <Loader />
                 ) : (
                     <div className="w-full relative">
-                        <div className={`${active ? "bg-[#ffffff] dark:bg-opacity-50 dark:bg-gradient-to-b dark:from-gray-900 dark:to-black fixed top-0 left-0 w-full h-[80px] z-[80] border-b dark:border-[#ffffff1c] shadow-x1 transition duration-500"
-                            : "w-full border-b dark:border-[#ffffff1c] h-[80px] z-[80] dark:shadow"}`}>
+                        <div className={`${active ? "bg-[#ffffff] dark:bg-opacity-50 dark:bg-gradient-to-b dark:from-gray-900 dark:to-black fixed top-0 left-0 w-full h-[80px] z-[2000] border-b dark:border-[#ffffff1c] shadow-x1 transition duration-500"
+                            : "w-full border-b dark:border-[#ffffff1c] h-[80px] z-[2000] dark:shadow"}`}>
 
                             <div className="w-[95%] 800px:w-[92%] m-auto py-2 h-full">
                                 <div className="w-full h-[80px] flex items-center justify-between p-3">
