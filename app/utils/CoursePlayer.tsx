a791584a6b1030aca77db2bc489265f436c7a0ac
Adelaja Paul <pauladelaja54@gmail.com>
2024-11-30 16:01:30 +0100

    On the resume play

--- app/utils/CoursePlayer.tsx
+++ app/utils/CoursePlayer.tsx
@@ -1,4 +1,4 @@
-import React, { FC, useEffect, useRef, useState } from 'react';
+import React, { FC, useEffect, useState } from 'react';
 import axios from "axios"
 
 type Props = {
@@ -12,7 +12,6 @@ const CoursePlayer: FC<Props> = ({ videoUrl }) => {
         playbackInfo: "",
     });
 
-    const playerRef = useRef<HTMLIFrameElement | null>(null);
 
     useEffect(() => {
         axios.post("https://elearningapi-rjgb.onrender.com/api/v1/getVdoCipherOTP", {
@@ -22,38 +21,6 @@ const CoursePlayer: FC<Props> = ({ videoUrl }) => {
         });
     }, [videoUrl]);
 
-    useEffect(() => {
-        const savedPosition = localStorage.getItem(`lastPosition-${videoUrl}`);
-        if (savedPosition && playerRef.current) {
-            const player = playerRef.current.contentWindow;
-            if (player) {
-                // Wait for the iframe player to initialize and then seek to the last position
-                player.postMessage(
-                    {
-                        event: 'seek',
-                        time: parseFloat(savedPosition),
-                    },
-                    '*'
-                );
-            }
-        }
-    }, [videoData]);
-
-    const handleTimeUpdate = (event: MessageEvent) => {
-        const data = event.data;
-        if (data.event === 'timeupdate') {
-            // Save current time to localStorage
-            localStorage.setItem(`lastPosition-${videoUrl}`, data.time.toString());
-        }
-    };
-
-    useEffect(() => {
-        // Listen for messages from the VdoCipher iframe
-        window.addEventListener('message', handleTimeUpdate);
-        return () => {
-            window.removeEventListener('message', handleTimeUpdate);
-        };
-    }, []);
 
     /*useEffect(() => {
         axios.post("http://localhost:8000/api/v1/getVdoCipherOTP",{
@@ -67,7 +34,7 @@ const CoursePlayer: FC<Props> = ({ videoUrl }) => {
         <div style={{ paddingTop: "56.25%", position: "relative", overflow: "hidden" }}>
             {
                 videoData.otp && videoData.playbackInfo !== "" && (
-                    <iframe src={`https://player.vdocipher.com/v2/?otp=${videoData?.otp}&playbackInfo=${videoData.playbackInfo}&player=76JQrEzivLboEVug`}
+                    <iframe src={`https://player.vdocipher.com/v2/?otp=${videoData?.otp}&playbackInfo=${videoData.playbackInfo}&player=76JQrEzivLboEVug&enableAutoResume=true`}
                         style={{
                             border: 0,
                             width: "100%",
